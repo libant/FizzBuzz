37a5fe6d6a4a2ee43db88733a01eb6c0016d3ccc
libant <liban.timir@mail.utoronto.ca>
2024-05-16 18:37:13 -0400

    testing

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -10,6 +10,7 @@ class FizzBuzz {
             // Find out which numbers divide i.
             boolean divisibleBy3 = i % 3 == 0;
             boolean divisibleBy5 = i % 5 == 0;
+            // testing for commiting
 
             // Print our appropriate result.
             if (divisibleBy3 && divisibleBy5) {
